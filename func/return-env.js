c84e9e85a8d62a582f093a0d0cc2c700a55954d8
soarflat <soarflat777@gmail.com>
2018-06-08 20:32:52 +0900

    initial commit

---
+++ func/return-env.js
@@ -0,0 +1,9 @@
+exports.handler = function(event, context, callback) {
+  const { FAKE_TOKEN } = process.env;
+  console.log(FAKE_TOKEN);
+  // カスタムヘッダのないコールバック
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ token: FAKE_TOKEN }),
+  });
+};
