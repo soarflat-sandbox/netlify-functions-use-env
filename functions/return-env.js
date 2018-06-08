97d91d953b2b319c5c0ee0305158377fdd7fc895
soarflat <soarflat777@gmail.com>
2018-06-08 20:48:37 +0900

    Revert "added process.env log"
    This reverts commit 4f7bb76a956fb2eb115db658cb867b02b6fb0400.

--- functions/return-env.js
+++ functions/return-env.js
@@ -1,6 +1,5 @@
 exports.handler = function(event, context, callback) {
   const { FAKE_TOKEN } = process.env;
-  console.log(process.env);
   console.log(FAKE_TOKEN);
   // カスタムヘッダのないコールバック
   callback(null, {
